c1bfb89d878c93d70b29398cc930ceb9fdf29f9a
Reeve/Xwy <18086052+reeve567@users.noreply.github.com>
2023-05-28 23:15:54 -0400

    add stuff

---
+++ config/plugins.js
@@ -0,0 +1,24 @@
+module.exports = ({ env }) => ({
+  // enable a plugin that doesn't require any configuration
+  i18n: true,
+
+  email: {
+    config: {
+      provider: "strapi-provider-email-resend",
+      providerOptions: {
+        apiKey: env("RESEND_API_KEY"), // Required
+      },
+      settings: {
+        defaultFrom: "no-reply@reeve.dev",
+        defaultReplyTo: "no-reply@reeve.dev",
+      },
+    }
+  },
+  
+  graphql: {
+    enabled: true,
+    config: {
+      playgroundAlways: false,
+    }
+  },
+});
